8700eb6dd49f08660ec52fa61ca01622d339c57b
W3NDO <wendonyang@gmail.com>
2024-07-25 18:26:44 +0200

    chore(main) - changes on routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  # get 'auction_item/new'
-  # get 'auction_item/index'
-  # get 'bid/new'
-  # get 'bid/index'
-  # get 'user/new'
-  # get 'home/index'
+  get 'auction_item/new'
+  get 'auction_item/index'
+  get 'bid/new'
+  get 'bid/index'
+  get 'user/new'
+  get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
